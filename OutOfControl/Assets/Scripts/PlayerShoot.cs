13089419bb2d56549cdcf8d591c95014c31ceaf1
Jake Parente <jakeparente@gmail.com>
2020-07-10 16:35:08 -0600

    basic shooting and level layout

---
+++ OutOfControl/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    private float bulletForce = 20f;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.transform.parent.rotation * Quaternion.Euler(0,0,90f));
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.right * bulletForce, ForceMode2D.Impulse);
+    }
+}
