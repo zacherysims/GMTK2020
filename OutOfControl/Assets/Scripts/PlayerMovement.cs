0a0fb5622c76c18501f5e28c9fd496f66c244651
Sunny Nagam <sunnynagam1@gmail.com>
2020-07-11 16:54:19 -0600

    merge?

--- OutOfControl/Assets/Scripts/PlayerMovement.cs
+++ OutOfControl/Assets/Scripts/PlayerMovement.cs
@@ -6,16 +6,34 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public bool isGrounded = false;
+    public float leftConstraint = Screen.width;
+    public float rightConstraint = Screen.width;
+    public Camera cam;
+    public float distanceZ;
 
     private Rigidbody2D rb;
 
     void Start()
     {
+        cam = Camera.main;
+        distanceZ = Mathf.Abs(cam.transform.position.z + transform.position.z);
+        leftConstraint = cam.ScreenToWorldPoint(new Vector3(0.0f, 0.0f, distanceZ)).x;
+        rightConstraint = cam.ScreenToWorldPoint(new Vector3(Screen.width, 0.0f, distanceZ)).x;
         rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
+        if(transform.position.x < leftConstraint)
+        {
+            transform.position = new Vector3(rightConstraint, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > rightConstraint)
+        {
+            transform.position = new Vector3(leftConstraint, transform.position.y, transform.position.z);
+        }
+
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * moveSpeed;
 
--- OutOfControl/Assets/Scripts/UIController.cs
+++ OutOfControl/Assets/Scripts/UIController.cs
@@ -3,17 +3,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour
 {
-    public Transform Player;
-    public GameObject floatingPoints;
+    public Transform player;
+    public GameObject gameOverPanel;
 
     public Text scoreText;
+    public Text highScoreText;
     [HideInInspector]
     public int score;
+    [HideInInspector]
+    public int highScore;
 
     public Slider confidenceSlider;
+
+    private void Start()
+    {
+        highScore = PlayerPrefs.GetInt("HighScore");
+        highScoreText.text = "High Score: " + highScore.ToString();
+        Time.timeScale = 1f;
+    }
 
     // Update is called once per frame
     void Update()
@@ -24,13 +35,31 @@ public class UIController : MonoBehaviour
     public void IncreaseScore(int points)
     {
         score += (int)(1 / confidenceSlider.value * points);
-        floatingPoints.GetComponentInChildren<TextMesh>().text = "+" + (int)(1 / confidenceSlider.value * points);
-        Instantiate(floatingPoints, new Vector3(-7f,5f), Quaternion.identity);
         confidenceSlider.value += 0.1f;
     }
 
     public void DecreaseConfidence()
     {
         confidenceSlider.value -= 0.25f;
+        if (confidenceSlider.value <= 0f)
+            GameOver();
+    }
+
+    public void GameOver()
+    {
+        if (score > PlayerPrefs.GetInt("HighScore"))
+        {
+            highScoreText.text = "High Score: " + score.ToString();
+            PlayerPrefs.SetInt("HighScore", score);
+        }
+
+        Destroy(player.gameObject);
+        gameOverPanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void ReloadGame()
+    {
+        SceneManager.LoadScene(UnityEngine.Random.Range(0, SceneManager.sceneCount));
     }
 }
